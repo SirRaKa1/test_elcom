181d646ce2da1dd152f4be69fe7228bf2e07ca1f
RaKaLuS <sir.d-i-a2014@mail.ru>
2024-03-14 13:16:36 +0700

    First version

--- test_elcom/Program.cs
+++ test_elcom/Program.cs
@@ -1,2 +1,50 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using test_elcom;
+
+Console.WriteLine("Hello! Write the name for your tamagotchi");
+Tamagotchi tamagotchi = new Tamagotchi(Console.ReadLine());
+string state = tamagotchi.Name + " is idle";
+
+while (tamagotchi.Health > 0)
+{
+    Console.Clear();
+    Console.WriteLine(tamagotchi.ToString(state));
+    Console.WriteLine("1. Feed\n2. Play\n3. Sleep\n4. Heal");
+    try{
+        int s = Convert.ToInt32(Console.ReadLine());
+        switch (s)
+        {
+            case 1: 
+                tamagotchi.Feed();
+                state = tamagotchi.Name + " has eated";
+                break;
+            case 2: 
+                tamagotchi.Play();
+                state = tamagotchi.Name + " played with you";
+                break;
+            case 3: 
+                tamagotchi.Sleep();
+                state = tamagotchi.Name + " has slept";
+                break;
+            case 4: 
+                tamagotchi.Heal();
+                state = tamagotchi.Name + " has healed";
+                break;
+            default:
+                Console.Clear();
+                Console.WriteLine("Use numbers from the above\nPress any key to continue"); 
+                Console.ReadKey();
+                break;
+        }
+        if (tamagotchi.Health < 3) state = state + "\n" + tamagotchi.Name + " is almost dead";
+    }
+    catch (Exception ex) {
+        Console.Clear();
+        Console.WriteLine("Use numbers from the above\nPress any key to continue");
+        Console.ReadKey();
+    }
+}
+Console.Clear();
+Console.WriteLine(tamagotchi.Name + " is dead");
+Console.ReadKey();
+
---
+++ test_elcom/Tamagotchi.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_elcom
+{
+    internal class Tamagotchi
+    {
+        public Tamagotchi(string name)
+        {
+            Name = name;
+            Health = 10;
+            Happiness = 5;
+            Hunger = 0;
+            Tiredness = 0;
+        }
+
+
+        public string Name;
+
+        public int Health;
+
+        public int Happiness;
+
+        public int Hunger;
+
+        public int Tiredness;
+
+        public string  ToString(String state)
+        {
+            StringBuilder s = new(Name);
+            s.AppendLine("\n\n" + state)
+                .AppendLine("\n\nHealth")
+                .AppendLine(StatBar(Health))
+                .AppendLine("Happiness")
+                .AppendLine(StatBar(Happiness))
+                .AppendLine("Hunger")
+                .AppendLine(StatBar(Hunger))
+                .AppendLine("Tiredness")
+                .AppendLine(StatBar(Tiredness));
+            return s.ToString();
+        }
+
+        private string StatBar(int x)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < x; i++) { sb.Append("#"); }
+            for (int i = x; i<10; i++) { sb.Append('_'); }
+            return sb.ToString();
+        }
+
+        public void Sleep()
+        {
+            Health++;
+            if (Health > 10) { Health = 10; }
+            Tiredness = 0;
+            Hunger++;
+            Hunger = UpCheck(Hunger);
+            Happiness -= 3;
+            Happiness = DownCheck(Happiness);
+        }
+
+        public void Feed()
+        {
+            Tiredness++;
+            Tiredness = UpCheck(Tiredness);
+            Hunger -= 5;
+            Hunger = DownCheck(Hunger);
+            Happiness -= 2;
+            Happiness = DownCheck(Happiness);
+        }
+
+        public void Play()
+        {
+            Tiredness += 3;
+            Tiredness = UpCheck(Tiredness);
+            Hunger += 3;
+            Hunger = UpCheck(Hunger);
+            Happiness += 5;
+            if(Happiness > 10) {  Happiness = 10; }
+        }
+
+        public void Heal()
+        {
+            Health = 10;
+            Hunger += 5;
+            Tiredness += 5;
+            Happiness -= 5;
+            if ((Tiredness > 10) && (Hunger > 10) && (Happiness < 10)) { Health = 0; }
+            if (Hunger > 10) { Hunger = 10; }
+            if (Tiredness > 10) { Tiredness = 10; }
+            if(Happiness < 0) { Happiness = 0; }
+        }
+
+        private int DownCheck(int x)
+        {
+            if (x < 0)
+            {
+                Health -= 3;
+                x = 0;
+            }
+            return x;
+
+        }
+
+        private int UpCheck(int x)
+        {
+            if (x > 10)
+            {
+                Health -= 3;
+                x= 10;
+            }
+            return x;
+                
+        }
+    }
+}
